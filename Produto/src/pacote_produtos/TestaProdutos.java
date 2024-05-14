1193526a7e35e42adcc8c07274538d150736077d
Vinicius Camargo <111100532+vinicamargors@users.noreply.github.com>
2024-05-14 20:40:16 -0300

    Add files via upload

--- Produto/src/pacote_produtos/TestaProdutos.java
+++ Produto/src/pacote_produtos/TestaProdutos.java
@@ -4,13 +4,21 @@ public class TestaProdutos {
 	
 	public static void main (String[] args) {
 		Produtos p1 = new Produtos();
+		Produtos p2 = new Produtos();
+		
 		p1.getCodProd();
 		p1.getNomeProd();
 		p1.getPrecoProd();
 		p1.getQtdProd();
 		
+		p2.setCodProd(1234);
+		p2.setNomeProd("Blusa de frio");
+		p2.setPrecoProd(89.99);
+		p2.setQtdProd(56);
+		
 		System.out.println(p1);
-	
-	}
-	
+		System.out.println();
+		System.out.println(p2);
+
+}
 }
